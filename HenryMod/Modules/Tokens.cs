2f4f70bf8234b7ca44ffdbe8b2d81d32cfe0e3d6
SwiggettDJ <54293785+SwiggettDJ@users.noreply.github.com>
2022-05-13 02:37:48 -0600

    Initial commit

---
+++ HenryMod/Modules/Tokens.cs
@@ -0,0 +1,67 @@
+﻿using R2API;
+using System;
+
+namespace EkkoMod.Modules
+{
+    internal static class Tokens
+    {
+        internal static void AddTokens()
+        {
+            #region Ekko
+            string prefix = EkkoPlugin.DEVELOPER_PREFIX + "_HENRY_BODY_";
+
+            string desc = "Ekko is a prodigy who invented the Z-Drive which he uses to manipulate time to twist any situation to his advantage.<color=#CCD3E0>" + Environment.NewLine + Environment.NewLine;
+            desc = desc + "< ! > Sword is a good all-rounder while Boxing Gloves are better for laying a beatdown on more powerful foes." + Environment.NewLine + Environment.NewLine;
+            desc = desc + "< ! > Pistol is a powerful anti air, with its low cooldown and high damage." + Environment.NewLine + Environment.NewLine;
+            desc = desc + "< ! > Roll has a lingering armor buff that helps to use it aggressively." + Environment.NewLine + Environment.NewLine;
+            desc = desc + "< ! > Bomb can be used to wipe crowds with ease." + Environment.NewLine + Environment.NewLine;
+
+            string outro = "..and so he left, searching for a new identity.";
+            string outroFailure = "..and so he vanished, forever a blank slate.";
+
+            LanguageAPI.Add(prefix + "NAME", "Ekko");
+            LanguageAPI.Add(prefix + "DESCRIPTION", desc);
+            LanguageAPI.Add(prefix + "SUBTITLE", "The Boy Who Shattered Time");
+            LanguageAPI.Add(prefix + "LORE", "sample lore");
+            LanguageAPI.Add(prefix + "OUTRO_FLAVOR", outro);
+            LanguageAPI.Add(prefix + "OUTRO_FAILURE", outroFailure);
+
+            #region Skins
+            LanguageAPI.Add(prefix + "DEFAULT_SKIN_NAME", "Default");
+            LanguageAPI.Add(prefix + "MASTERY_SKIN_NAME", "Alternate");
+            #endregion
+
+            #region Passive
+            LanguageAPI.Add(prefix + "PASSIVE_NAME", "Z-Drive Resonance");
+            LanguageAPI.Add(prefix + "PASSIVE_DESCRIPTION", "Ekko's basic attacks and damaging abilities apply a Resonance stack to affected enemies, stacking up to 3 times. /n At 3 stacks, Resonance is consumed to deal 300% damage.");
+            #endregion
+
+            #region Primary
+            LanguageAPI.Add(prefix + "PRIMARY_SLASH_NAME", "Clockhand Bat");
+            LanguageAPI.Add(prefix + "PRIMARY_SLASH_DESCRIPTION", Helpers.agilePrefix + $"Swing forward for <style=cIsDamage>{100f * StaticValues.swordDamageCoefficient}% damage</style>.");
+            #endregion
+
+            #region Secondary
+            LanguageAPI.Add(prefix + "SECONDARY_GUN_NAME", "Timewinder");
+            LanguageAPI.Add(prefix + "SECONDARY_GUN_DESCRIPTION", Helpers.agilePrefix + $"Fire a handgun for <style=cIsDamage>{100f * StaticValues.gunDamageCoefficient}% damage</style>.");
+            #endregion
+
+            #region Utility
+            LanguageAPI.Add(prefix + "UTILITY_ROLL_NAME", "Phase Dive");
+            LanguageAPI.Add(prefix + "UTILITY_ROLL_DESCRIPTION", "Roll a short distance, gaining <style=cIsUtility>300 armor</style>. <style=cIsUtility>You cannot be hit during the roll.</style>");
+            #endregion
+
+            #region Special
+            LanguageAPI.Add(prefix + "SPECIAL_BOMB_NAME", "Chronobreak");
+            LanguageAPI.Add(prefix + "SPECIAL_BOMB_DESCRIPTION", $"Throw a bomb for <style=cIsDamage>{100f * StaticValues.bombDamageCoefficient}% damage</style>.");
+            #endregion
+
+            #region Achievements
+            LanguageAPI.Add(prefix + "MASTERYUNLOCKABLE_ACHIEVEMENT_NAME", "Ekko: Mastery");
+            LanguageAPI.Add(prefix + "MASTERYUNLOCKABLE_ACHIEVEMENT_DESC", "As Ekko, beat the game or obliterate on Monsoon.");
+            LanguageAPI.Add(prefix + "MASTERYUNLOCKABLE_UNLOCKABLE_NAME", "Ekko: Mastery");
+            #endregion
+            #endregion
+        }
+    }
+}
