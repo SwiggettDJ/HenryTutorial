e8581a0c03ed083164923f21bb1ad9d169c5a72c
DJ <swiggettdj@gmail.com>
2022-05-19 14:32:44 -0600

    Ekko aniamtions work

---
+++ EkkoMod/Modules/ItemDisplaysBase.cs
@@ -0,0 +1,19 @@
+﻿using RoR2;
+using System.Collections.Generic;
+
+namespace EkkoMod.Modules.Characters {
+    public abstract class ItemDisplaysBase {
+
+        public void SetItemDisplays(ItemDisplayRuleSet itemDisplayRuleSet) {
+
+            List<ItemDisplayRuleSet.KeyAssetRuleGroup> itemDisplayRules = new List<ItemDisplayRuleSet.KeyAssetRuleGroup>();
+
+            SetItemDisplayRules(itemDisplayRules);
+
+            itemDisplayRuleSet.keyAssetRuleGroups = itemDisplayRules.ToArray();
+            itemDisplayRuleSet.GenerateRuntimeValues();
+        }
+
+        protected abstract void SetItemDisplayRules(List<ItemDisplayRuleSet.KeyAssetRuleGroup> itemDisplayRules);
+    }
+}
